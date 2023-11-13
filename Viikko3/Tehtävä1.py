75f254c0951870ccdda5ea6a47602a8fd9ff2b87
Itz-Ezu <eetu.oinonen@metropolia.fi>
2023-11-14 00:38:27 +0200

    Viikko3: tehtävät 1-3

---
+++ Viikko3/Tehtävä1.py
@@ -0,0 +1,87 @@
+from matplotlib import pyplot as plt, patches
+import numpy as np
+from fractions import Fraction as fr
+
+import matplotlib as mpl
+
+fig = plt.figure()
+ax = fig.subplots()
+
+ymp = patches.Circle((0, 0), radius=1, fill=0)
+ax.add_patch(ymp)
+
+# Move left y-axis and bottom x-axis to centre, passing through (0,0)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+
+# Eliminate upper and right axes
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+# Show ticks in the left and lower axes only
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+ax.axis('equal')
+
+plt.xticks([-1, 0, 1], minor=True)
+plt.yticks([-1, 0, 1])
+
+pist_xy=np.array([np.pi/6, np.pi/4, np.pi/3, np.pi/2, np.pi*2/3, np.pi*3/4, np.pi*5/6, np.pi, np.pi*1.5])
+varit=np.array(['red', 'green', 'yellow', 'blue', 'purple', 'brown', 'orange', 'pink', 'gray'])
+
+lista = [np.pi/6, np.pi/4, np.pi/3, np.pi/2, np.pi*2/3, np.pi*3/4, np.pi*5/6, np.pi, np.pi*1.5]
+
+x = np.cos(pist_xy)
+y = np.sin(pist_xy)
+
+plt.scatter(x, y, color=varit, marker='X')
+
+
+plt.annotate(r'$(\frac{\pi}{6})$',
+             xy=(np.cos(np.pi/6), np.sin(np.pi/6)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi}{4})$',
+             xy=(np.cos(np.pi/4), np.sin(np.pi/4)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi}{3})$',
+             xy=(np.cos(np.pi/3), np.sin(np.pi/3)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi}{2})$',
+             xy=(np.cos(np.pi/2), np.sin(np.pi/2)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi*2}{3})$',
+             xy=(np.cos(np.pi*2/3), np.sin(np.pi*2/3)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi*3}{4})$',
+             xy=(np.cos(np.pi*3/4), np.sin(np.pi*3/4)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi*5}{6})$',
+             xy=(np.cos(np.pi*5/6), np.sin(np.pi*5/6)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi}{1})$',
+             xy=(np.cos(np.pi), np.sin(np.pi)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+plt.annotate(r'$(\frac{\pi*1.5}{1})$',
+             xy=(np.cos(np.pi*1.5), np.sin(np.pi*1.5)), xycoords='data',
+             xytext=(+30, +0), textcoords='offset points', fontsize=12,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"))
+
+
+plt.show()
---
+++ Viikko3/Tehtävä2ja3.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(-np.pi*3, np.pi*3, 256, endpoint=True)
+C,S = np.cos(X), np.sin(X)
+
+plt.plot(X, C, color="green", linewidth=2.5, linestyle="--", label="cosine")
+plt.plot(X, S, color="orange",  linewidth=2.5, linestyle="--", label="sine")
+
+plt.legend(loc='upper left', frameon=False)
+
+plt.xlim(X.min()*1, X.max()*1)
+plt.ylim(C.min()*1, C.max()*1)
+
+plt.xticks( [-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
+plt.yticks([-1, 0, +1])
+
+plt.xticks([-np.pi*3, -np.pi*5/2, -np.pi*2, -np.pi*3/2, -np.pi, -np.pi/2, 0, np.pi/2, np.pi, np.pi*1.5, np.pi*2, np.pi*2.5, np.pi*3],
+       [r'$-3\pi$', r'$-2.5\pi$', r'$-2\pi$', r'$-1.5\pi$', r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$', r'$+1.5\pi$', r'$+2\pi$',  r'$+2.5\pi$', r'$+3\pi$'])
+
+plt.yticks([-1, -0.50, 0, 0.50, +1],
+       [r'$-1$', r'$-0.50$', r'$0$', r'$0.50$', r'$+1$'])
+
+
+plt.plot(X,C)
+plt.plot(X,S)
+
+plt.show()
